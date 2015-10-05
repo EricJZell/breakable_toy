f910dddea1ecc9292e92b2cbe84b6546ccd9b22c
Eric Zell <EricJZell@gmail.com>
2015-10-05 18:16:02 -0400

    fix hound errors

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -5,7 +5,7 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   #   storage :file
   # end
   storage :file
-  
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
