aa39f283b4d7199c4265ec6cec58304e3916a45f
Eric Zell <EricJZell@gmail.com>
2015-10-05 18:04:47 -0400

    change photo storage

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -1,10 +1,11 @@
 class ProfilePhotoUploader < CarrierWave::Uploader::Base
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
-
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
+  storage :file
+  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
