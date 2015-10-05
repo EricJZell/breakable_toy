c913389b4443dee665476cd0f0310ac1d29173ef
Eric Zell <EricJZell@gmail.com>
2015-10-05 14:39:05 -0400

    user can visit profile page

---
+++ spec/features/user_visits_profile_page_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'user visits profile page', %Q{
+  As an authenticated user
+  I want to view my profile page
+  So that I can see how it looks
+
+  # Acceptance Criteria
+  [] I must be signed in
+  [] I must navigate to my profile from the home page
+
+} do
+
+  scenario 'authenticated user signs out' do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    click_link 'View Your Profile'
+    expect(current_path).to eq("/users/#{user.id}")
+  end
+
+end
