eb1d3339d880fbd8675e42a7d38255305f342ff5
Eric Zell <EricJZell@gmail.com>
2015-10-09 17:06:19 -0400

    google maps works

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -31,20 +31,20 @@ $("#country-select").on("change", function() {
 
 
 function initialize() {
-  var locations = $('#map-canvas').data("locations");
+  var locations = $("#map-canvas").data("locations");
   //debugger;
   var mapProp = {
     center:{lat: 40, lng: 290},
     zoom:1,
-    backgroundColor: '#0e1525',
+    backgroundColor: "#0e1525",
     mapTypeId:google.maps.MapTypeId.ROADMAP
   };
   var map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
   for (var i = 0; i < locations.length; i++) {
     new google.maps.Marker({
-      position:{lat: locations[i]['lat'], lng: locations[i]['lon']},
+      position:{lat: locations[i].lat, lng: locations[i].lon},
       map: map,
-      title: 'hello, world'
+      title: "hello, world"
     });
   };
   var styles = [
@@ -79,4 +79,4 @@ function initialize() {
 
   map.setOptions({styles: styles});
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, "load", initialize);
