2e48d5e9e7f1ee28e5651414ead2727baca240bb
Eric Zell <EricJZell@gmail.com>
2015-10-10 14:57:19 -0400

    photos are uploading for entries

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,5 @@ class Photo < ActiveRecord::Base
   validates :file_name, presence: true
   validates :entry_id, presence: true
   belongs_to :entry
+  mount_uploader :file_name, EntryPhotoUploader
 end
