623d17fc9a99d2929064fe8a31b7ccd1f8670664
Eric Zell <EricJZell@gmail.com>
2015-10-11 15:01:36 -0400

    add highcharts for activity

---
+++ app/assets/javascripts/charts.js
@@ -0,0 +1,25 @@
+$(function () {
+  var activityChart = $('#activity-chart');
+  var months = activityChart.data().months
+  var sessions = activityChart.data().sessions
+  activityChart.highcharts({
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Activity'
+    },
+    xAxis: {
+      categories: months
+    },
+    yAxis: {
+      title: {
+        text: 'Sessions'
+      }
+    },
+    series: [{
+      name: 'Entries per month',
+      data: sessions
+    }]
+  });
+});
