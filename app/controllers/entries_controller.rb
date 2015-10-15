d67266344493463a3ac4fba9d34939adfc16b205
Eric Zell <EricJZell@gmail.com>
2015-10-15 16:06:15 -0400

    making a new seeder

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -102,27 +102,18 @@ class EntriesController < ApplicationController
   end
 
   def distance_miles(lat1, lon1, lat2, lon2)
-    #rmiles = radius of earth in miles
     rmiles = 3956
     rad_per_deg = 0.017453293
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-
     dlon_rad = dlon * rad_per_deg
     dlat_rad = dlat * rad_per_deg
-
     lat1_rad = lat1 * rad_per_deg
-    #lon1_rad = lon1 * rad_per_deg
-
     lat2_rad = lat2 * rad_per_deg
-    #lon2_rad = lon2 * rad_per_deg
-
     a = (Math.sin(dlat_rad / 2))**2 + Math.cos(lat1_rad) *
       Math.cos(lat2_rad) * (Math.sin(dlon_rad / 2))**2
     c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-
-    dMi = rmiles * c
-    return dMi
+    rmiles * c
   end
 
 end
