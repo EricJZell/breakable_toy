e6f5a3ead8d0b8bb6311d7f94590dc2ede92418c
Eric Zell <EricJZell@gmail.com>
2015-10-20 23:53:01 -0400

    I made friends

--- db/migrate/20151014185417_create_friendships.rb
+++ db/migrate/20151014185417_create_friendships.rb
@@ -1,8 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-      t.integer :requestor_id, null: false
-      t.integer :requestee_id, null: false
+      t.integer :user_id, null: false
+      t.integer :friend_id, null: false
       t.boolean :accepted, null: false, default: false
     end
   end
